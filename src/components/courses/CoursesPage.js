9fc3c1059f5125e25bcc6b4a35da538713568c3b
itsrealoj <stephenojwang1040@gmail.com>
2020-04-18 11:35:44 +0300

    added form functions

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React from 'react';
 
 class CoursesPage extends React.Component {
+  state = {
+    courses: {
+      tittle: '',
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, tittle: event.target.value };
+    this.setState({ course });
+  };
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type='text'
+          onChange={this.handleChange}
+          value={this.state.courses.title}
+        />
+        <input type='submit' value='save' />
+      </form>
+    );
   }
 }
 
