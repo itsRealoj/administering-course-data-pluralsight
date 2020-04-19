76525cddc490f88c9a1c855bab43ad724a19e7ca
itsrealoj <stephenojwang1040@gmail.com>
2020-04-20 00:36:41 +0300

    displaying course lists

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => (
   <div className='jumbotron'>
     <h1>Karean Administration</h1>
-    <p>Using React, Redux and React Router for ultra-responsive web apps.</p>
+    <p>Manage courses</p>
     <Link to='about' className='btn btn-primary btn-lg'>
       Learn more
     </Link>
